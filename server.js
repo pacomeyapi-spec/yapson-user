02768378c1c02815e85b32d9190353e7cb74814a
pacomeyapi-spec <pacomeyapi@gmail.com>
2026-05-13 22:31:10 +0000

    fix: use process.env.PORT, add railway.toml healthcheck

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
